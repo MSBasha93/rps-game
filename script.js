61f38815e09471451c287cf9accd0d80cca93843
MBasha <55190972+MSBasha93@users.noreply.github.com>
2024-11-01 07:46:38 +0200

    Create basic structure and pseudocode
    Pseudocode is based on the GUI, game announcment is
    text based in HTML

---
+++ script.js
@@ -0,0 +1,32 @@
+/*
+Pseudocode:
+newGame{
+    reset score to zero
+    show choices
+    annouce start of game
+}
+
+While score < max {
+    Get userChoice
+    Generate computerChoice
+    Compare choices
+    Update score
+    Annouce userCoice and computerChoice and roundWinner
+}
+
+
+If score = max {
+    Announce game winenr
+    Hide choices
+}
+
+
+
+*/
+
+
+const rock = document.getElementById('rock');
+const paper = document.getElementById('rock');
+const scissors = document.getElementById('scissors');
+
+
