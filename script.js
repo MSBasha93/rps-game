c614b6f01be4cbc1a3c998cb1d1e31c2929b7a63
MBasha <55190972+MSBasha93@users.noreply.github.com>
2024-11-06 07:21:27 +0200

    Add start game function
    Created newGame fn and linked to newGameBtn.

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ Pseudocode:
 newGame{
     reset score to zero
     show choices
+    show score
     annouce start of game
+
 }
 
 While score < max {
@@ -24,9 +26,35 @@ If score = max {
 
 */
 
-
+let userScore=0;
+let userChoice="";
+let computerScore=0;
+let computerChoice="";
+const score = document.getElementById('score');
+const announce= document.getElementById("announce");
+const choices=document.getElementById('choice');
 const rock = document.getElementById('rock');
 const paper = document.getElementById('rock');
 const scissors = document.getElementById('scissors');
+const newGameBtn= document.getElementById('new-game');
+
+function newGame() {
+    userScore=0;
+    computerScore=0;
+    if (choices.style.display === "none") {
+        choices.style.display = "block"; //Show user choices
+      }
+    announce.innerText="A new game has started, Please Pick a choice";
+    score.innerText=`USER: ${userScore} \\ CPU: ${computerScore}`;
+}
+
+
+
+
+
+
+newGameBtn.addEventListener("click",newGame);
+
+
 
 
